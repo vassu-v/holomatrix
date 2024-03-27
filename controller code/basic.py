07730190f617fe089860260d8aab350bc05c5cc2
vassu-v <68139009+vassu-v@users.noreply.github.com>
2024-03-27 21:47:14 +0530

    Create basic.py
    for controlling

---
+++ controller code/basic.py
@@ -0,0 +1,4 @@
+#In this a basic python code will be created to register click on computer when captured by camera on a specified area where user can see the screen 
+import OpenCV
+import MediaPipe
+import pyautogui
